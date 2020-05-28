33ef430304cadae8acf1a3610ef603e6b621d1ad
Mike Marcacci <mike.marcacci@gmail.com>
2020-05-28 13:50:22 -0700

    Set additionalProperties = false by default

--- src/normalizer.ts
+++ src/normalizer.ts
@@ -50,7 +50,8 @@ rules.set('Transform `required`=false to `required`=[]', schema => {
 // TODO: default to empty schema (as per spec) instead
 rules.set('Default additionalProperties to true', schema => {
   if (!('additionalProperties' in schema) && isObjectType(schema) && schema.patternProperties === undefined) {
-    schema.additionalProperties = true
+    // By default, only enumerate defined fields...
+    schema.additionalProperties = false
   }
 })
 
